754a1411633894add7b853bf97b70d88e766845f
Yuan Yen Tai <admin@linkdea.com>
2025-07-16 16:29:12 +0000

    Update at 2025-07-16 16:29:12

--- test.py
+++ test.py
@@ -6,11 +6,9 @@ df = pd.read_parquet('metadata_speaker.parquet')
 df = df[~df['lines'].isna()]
 df
 # %%
-df.to_parquet('metadata_speaker.parquet')
-
-
+df
 # %%
-df = pd.read_parquet('metadata.parquet')
-# %%
+
+df = pd.read_parquet('metadata_speaker.parquet')
 df
 # %%
